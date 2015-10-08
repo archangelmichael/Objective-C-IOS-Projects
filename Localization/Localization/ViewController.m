cc1dc80c00772c0dec9fd920efc7a9cb77427217
Radi <radishikerov@yahoo.com>
2015-10-08 14:23:36 +0300

    Added german localization to labels, buttons and loading specific image depending on language.

--- Localization/Localization/ViewController.m
+++ Localization/Localization/ViewController.m
@@ -12,6 +12,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *labelHi;
 @property (weak, nonatomic) IBOutlet UILabel *labelAnswer;
 @property (weak, nonatomic) IBOutlet UIButton *buttonAsk;
+@property (weak, nonatomic) IBOutlet UIImageView *ivGreeting;
 
 @end
 
@@ -20,6 +21,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.labelAnswer.alpha = 0.0;
+    
+    //self.labelHi.text = [NSString stringWithFormat:NSLocalizedString(@"greeting", nil), @"username"];
+    self.labelAnswer.text = NSLocalizedString(@"answer", nil);
+    [self.buttonAsk setTitle:NSLocalizedString(@"question", nil) forState:UIControlStateNormal];
+    [self.ivGreeting setImage:[UIImage imageNamed:NSLocalizedString(@"greetingImage", nil)]];
 }
 
 - (void)didReceiveMemoryWarning {
